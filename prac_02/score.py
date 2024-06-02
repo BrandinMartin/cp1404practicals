aca95820eb9e3f900bc88b1829d29fbd9038eb4d
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-06-02 23:52:46 +1000

    Add updated score status program

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,32 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404 - prac_02
+Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    """Main function that asks the user for their score and prints the result then it gets a random score/result."""
+    score = float(input("Enter your score: "))
+    result = determine_status(score)
+    print(result)
+    random_score = random.randint(0, 100)
+    result = determine_status(random_score)
+    print(f"\nRandom score: {random_score}")
+    print(result)
+
+
+def determine_status(score):
+    """Determines the result based on the given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
