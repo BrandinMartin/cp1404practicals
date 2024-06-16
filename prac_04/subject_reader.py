48ef96956349580ce3609287191246449a419b43
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-06-16 21:35:48 +1000

    Add data loading and display function for subject data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,5 +1,5 @@
 """
-CP1404/CP5632 Practical
+CP1404 - prac_04
 Data file -> lists program
 """
 
@@ -8,22 +8,25 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    print_subject_details(data)
 
 
 def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Loads data from a text file and returns it as a nested list"""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def print_subject_details(data):
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 
 main()
