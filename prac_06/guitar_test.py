8190239363f13fac2a32535cf41f27f90f960f3e
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-07-14 23:27:09 +1000

    Add testing features for guitar class

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,7 +1,7 @@
 """
 CP1404 prac_06 - Testing guitar program
 Estimated TTC - 50 minutes
-Actual TTC -
+Actual TTC - 65 minutes
 """
 from guitar import Guitar
 
@@ -10,7 +10,7 @@ CURRENT_YEAR = 2024
 
 def test_guitar():
     """Tests Guitar class functionality"""
-    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)  # Couldn't work out how to use
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)  # Couldn't work out how to use differently
     another = Guitar("Another Guitar", 2013, 10000.10)
 
     print(f"{gibson.name} get_age() - Expected {100}. Got {gibson.get_age()}")
@@ -19,6 +19,9 @@ def test_guitar():
     print(f"{gibson.name} is_vintage() - Expected {True}. Got {gibson.is_vintage()}")
     print(f"{another.name} get_age() - Expected {False}. Got {another.is_vintage()}")
 
+
+if __name__ == '__main__':  # Allows the test to run
+    test_guitar()
 
 
 
