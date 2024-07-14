a9dae665fbc8390d95a1bd656451653cc9615254
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-07-14 23:57:18 +1000

    Add almost complete guitars.py program

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -15,7 +15,7 @@ def main():
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
-        cost = float(input("Cost: $"))
+        cost = float(input("Cost: "))
         new_guitar = Guitar(name, year, cost)
         guitars.append(new_guitar)
         print(f"{new_guitar} added.\n")
@@ -25,6 +25,13 @@ def main():
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
 
     print("These are my guitars: ")
-
+    if guitars:
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}
+    else:
+        print("No guitars :( Quick, go and buy one!")
 
 main()
