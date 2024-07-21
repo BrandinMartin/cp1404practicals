5d497dffbd17536e29be06f33b049f0fcfba3246
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-07-21 23:55:45 +1000

    Add read function

---
+++ prac_07/myguitars.py
@@ -0,0 +1,29 @@
+"""
+CP1404 prac_07
+"""
+
+from guitar import Guitar
+import csv
+
+FILENAME = 'guitars.csv'
+def main():
+    guitars = read_guitars(FILENAME)
+
+
+
+def read_guitars(filename):
+    """Read all the guitars and store them in a list of Guitar objects"""
+    guitars = []
+    with open(filename, 'r') as infile:
+        for line in infile:
+            parts = line.strip().split(',')
+            name = parts[0]
+            year = int(parts[1])
+            cost = float(parts[2].strip())
+            guitar = Guitar(name, year, cost)
+            guitars.append(guitar)
+    return guitars
+
+
+if __name__ == "__main__"
+    main()
