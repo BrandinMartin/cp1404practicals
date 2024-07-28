c5e75bd747dffa8b5fe3ec11ac86ecf1bb897b98
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-07-28 23:54:00 +1000

    Add functions and docstrings

--- prac_08/prac_08/KivyDemos-master/KivyDemos-master/convert_miles_km.py
+++ prac_08/prac_08/KivyDemos-master/KivyDemos-master/convert_miles_km.py
@@ -7,14 +7,35 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+FACTOR_MILES_TO_KM = 1.60934
 
-class ConvertMilesToKmApp(App):
-    message = StringProperty()
+class ConvertMilestoKm(App):
+    output_km = StringProperty("0.0")
 
     def build(self):
+        """Builds the app"""
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
+    def handle_calculate(self, text):
+        """Handles the calculation of miles to km"""
+        miles = self.convert_text_to_number(text)
+        self.update_result(miles)
 
-ConvertMilesToKmApp().run()
+    def handle_increment(self, text, change):
+        """Handles incrementation (either up or down)"""
+        miles = self.convert_text_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+
+    def update_result(self, miles):
+        self.output_km = str(miles * FACTOR_MILES_TO_KM)
+
+    def convert_text_to_number(self, text):
+        """Converts the text into a number """
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
+
+ConvertMilestoKm().run()
