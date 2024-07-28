12b213d736b77167d295291d7614206259e01d91
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-07-28 22:51:08 +1000

    Add class and build function

--- prac_08/prac_08/KivyDemos-master/KivyDemos-master/convert_miles_km.py
+++ prac_08/prac_08/KivyDemos-master/KivyDemos-master/convert_miles_km.py
@@ -3,3 +3,18 @@ CP1404 prac_08
 Converting miles to km program
 """
 
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+
+class ConvertMilesToKmApp(App):
+    message = StringProperty()
+
+    def build(self):
+        self.title = "Convert Miles to Kilometers"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+ConvertMilesToKmApp().run()
