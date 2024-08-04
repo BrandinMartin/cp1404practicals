39f191dd24800b09485672c03ef434d009ab933f
BrandinMartin <brandin.martin@my.jcu.edu.au>
2024-08-04 23:25:27 +1000

    Add menu functionality

--- prac_09/prac_09/taxi_simulator.py
+++ prac_09/prac_09/taxi_simulator.py
@@ -5,3 +5,40 @@ Taxi simulator
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    total = 0
+
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+
+    while choice != "q":
+        if choice == "c":
+            choose_taxi()
+        elif choice == "d":
+            if current_taxi:
+                current_taxi.start_fare()
+                distance = input("Drive how far? ")
+                current_taxi.drive(distance)
+                cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${cost:.2f}")
+                total += cost
+            else:
+                print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${total:.2f}")
+        print(MENU)
+        choice = input(">>> ").lower()
+
+
+def choose_taxi(taxis):
+    pass
+
+
+main()
